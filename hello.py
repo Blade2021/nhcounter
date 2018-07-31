eeeb8d06ec9963e10f089dab93e719724599f9e0
arthus3 <redhawk2021@live.com>
2018-07-31 14:23:30 -0400

    Nightly build (Working)

--- hello.py
+++ hello.py
@@ -1,21 +1,46 @@
 from tkinter import *
-from tkinter import messagebox
+root = Tk()
+counter = 20
 
-window = Tk()
-window.title("NAHA")
-window.geometry('350x200')
 
-lbl = Label(window, text="Hello")
-lbl.grid(column=0, row=0)
-txt = Entry(window, width=10)
+def makeSomething(value):
+    global counter
+    counter = value
+
+
+def printVariable():
+    print(counter)
+
+
+root.title("NAHA")
+root.geometry('350x200')
+txt = Entry(root, width=10)
 txt.grid(column=1, row=0)
 
 
-def clicked():
-    messagebox.showerror('Machine ERROR', 'Good job you just broke something.')
+def error():
+    messagebox.showerror('Machine Error', 'Good job you just broke something.')
 
 
-btn = Button(window, text="Click Me", command=clicked)
+def up():
+    makeSomething(counter+1)
+    printVariable()
+    lbl.configure(text=str(counter))
+
+
+def down():
+    makeSomething(counter-1)
+    printVariable()
+    lbl.configure(text=str(counter))
+
+
+lbl = Label(root, text="This is a test")
+lbl.grid(column=0, row=0)
+btn = Button(root, text="Click Me", command=error)
 btn.grid(column=2, row=0)
+upbtn = Button(root, text="Up", command=up())
+upbtn.grid(column=5, row=5)
+downbtn = Button(root, text="Down", command=down())
+downbtn.grid(column=6, row=5)
 
-window.mainloop()
+root.mainloop()
--- warning.py
+++ warning.py
@@ -1,3 +1,18 @@
-from tkinter import messagebox
+from tkinter import *
 
-messagebox.showinfo('Message title', 'Message content')
+root = Tk()
+countingLabel = Label(root, text="Time to Count:0", background='blue', foreground='white', font='Times 20', relief='groove', borderwidth=3)
+countingLabel.grid(row=0, column=1)
+theCountValue = 0
+
+
+def begin_count():
+    global theCountValue, countingLabel
+    theCountValue = theCountValue + 1
+    countingLabel.configure(text = "Count:" + str(theCountValue))
+
+
+incrementButton = Button(root,text="Increment",command=begin_count)
+incrementButton.grid(row=0, column=0)
+
+mainloop()
