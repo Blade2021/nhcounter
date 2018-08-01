15c7ccabefd16c7efc5a1017a75de52d30064f42
arthus3 <redhawk2021@live.com>
2018-08-01 14:35:23 -0400

    Major Update

--- warning.py
+++ warning.py
@@ -1,37 +1,44 @@
 from tkinter import *
 from tkinter import messagebox
+import configparser
 
 root = Tk()
-global maxValue
 maxValue = 100  # set a default
 theCountValue = 0
+root.title("NAHA Counting Machine")
+root.geometry('370x220')
+
+# Import configurations
+config = configparser.ConfigParser()
+config.read('data.ini')
+maxValue = int(config['DEFAULT']['KitAmount'])
+print("Read from INI File | Kit Amount: " + str(maxValue))
+
+
 countingLabel = Label(root, text="Time to Count:0", background='blue', foreground='white', font='Times 28',
                       relief='groove', borderwidth=3, width=12)
 countingLabel.grid(row=0, column=1)
 maxValueCount = Label(root, text="Kit Size:" + str(maxValue), background='blue', foreground='white', font='Times 28',
                       relief='groove', borderwidth=3, width=12)
 maxValueCount.grid(row=1, column=1)
-root.title("NAHA Counting Machine")
-root.geometry('370x220')
 
 
 def grabMax():
     value = txtEntry.get()
     if value.isnumeric():
         try:
-            print(value)
+            print("Updated Kit Amount to: " + value)
             finalValue = int(value)
             global maxValue
             maxValue = finalValue
             maxValueCount.configure(text="Kit Size:" + str(maxValue))
+            config['DEFAULT']['KitAmount'] = value
+            with open('data.ini', 'w') as configfile:
+                config.write(configfile)
             return int(value)
         except ValueError:
             return None
 
-
-def dispmax():
-    print(maxValue)
-    maxValueCount.configure(text="Kit Size:" + str(maxValue))
 
 
 def begin_count():
@@ -41,6 +48,10 @@ def begin_count():
         return
     theCountValue = theCountValue + 1
     countingLabel.configure(text="Count:" + str(theCountValue))
+
+
+def error():
+    messagebox.showerror('Machine Error', 'Good job you just broke something.')
 
 
 def lower_count():
@@ -62,7 +73,7 @@ txtEntry.grid(column=1, row=9)
 incrementButton = Button(root, text="Increment", width=10, command=begin_count)
 incrementButton.grid(row=0, column=0)
 
-decrementButton = Button(root, text="Decrement", width=10, command=lower_count)
+decrementButton = Button(root, text="Decrement", width=10, command=error)
 decrementButton.grid(row=1, column=0)
 
 resetButton = Button(root, text="Reset", width=10, command=reset_count)
@@ -71,8 +82,6 @@ resetButton.grid(row=2, column=0)
 grabMaxButton = Button(root, text="Enter", width=10, command=grabMax)
 grabMaxButton.grid(row=3, column=0)
 
-displayMax = Button(root, text="Display Max", width=10, command=dispmax)
-displayMax.grid(row=4, column=0)
 
 
 mainloop()
