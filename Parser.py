03b99216525ef00f4a0eaa7b50f63a2d12f5a3d4
arthus3 <redhawk2021@live.com>
2018-08-06 10:18:51 -0400

    Build

--- Parser.py
+++ Parser.py
@@ -1,5 +1,11 @@
-file = open("testfile.txt", "r+")
-for line in file:
-    print(line, end='')
-    if "How" in line:
-        file.write("However")
+import in_place
+
+value = "Changes"
+with in_place.InPlace('testfile.txt') as file:
+    line1 = file.readLine()
+    line2 = file.readLine()
+    for line in file:
+        if string1 == "Check":
+            line = line.replace(value, 'testZ')
+            file.write(line)
+        string1 = line
