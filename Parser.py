1dc98c25194131073c3536c0e6ff740d2fa33102
Blade2021 <Blade2021@live.com>
2018-08-06 14:07:43 -0400

    Update

--- Parser.py
+++ Parser.py
@@ -3,6 +3,7 @@ import sys
 
 x = 0
 y = 0
+rate = 0
 value = "Z-"
 
 
@@ -22,14 +23,20 @@ with fileinput.input(files="testfile.txt", backup="test.bak", inplace=1) as file
                 line = line[0:feedCheck]
             #line = feedCheck(line)
             line = line.rstrip('\n')
-            line += " F4. \n"
+            if rate >= 1:
+                line += " F4. \n"
+                rate = 0
+            else:
+                line += "\n"
         if (file.filelineno() == y) and ("G01" in line):
             feedCheck = line.find('F')
             if feedCheck >= 1:
                 line = line[0:feedCheck]
             #line = feedCheck(line)
-            line = line.rstrip('\n')
-            line += " F7. \n"
+            if rate == 0:
+                line = line.rstrip('\n')
+                line += " F7. \n"
+                rate = 1
             #line = " F7." + line
             #line = line.replace("Apple", "Orange")
         sys.stdout.write(line)
