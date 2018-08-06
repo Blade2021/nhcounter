d7f70bbdd7c8f632cc4bb4a58d1b4e83fc3e6a70
Blade2021 <Blade2021@live.com>
2018-08-06 13:59:34 -0400

    Update [ Working ]

--- Parser.py
+++ Parser.py
@@ -3,14 +3,35 @@ import sys
 
 x = 0
 y = 0
-value = "Pear"
+value = "Z-"
+
+
+def feedCheck(line):
+    feedCheck = line.find('F')
+    if feedCheck >= 1:
+        line = line[0:feedCheck]
+        return str(line)
+
 
 with fileinput.input(files="testfile.txt", backup="test.bak", inplace=1) as file:
     for line in file:
         if value in line:
             y = file.filelineno()+1
-        if (file.filelineno() == y) and ("Apple" in line):
-            line = line.replace("Apple", "Orange")
+            feedCheck = line.find('F')
+            if feedCheck >= 1:
+                line = line[0:feedCheck]
+            #line = feedCheck(line)
+            line = line.rstrip('\n')
+            line += " F4. \n"
+        if (file.filelineno() == y) and ("G01" in line):
+            feedCheck = line.find('F')
+            if feedCheck >= 1:
+                line = line[0:feedCheck]
+            #line = feedCheck(line)
+            line = line.rstrip('\n')
+            line += " F7. \n"
+            #line = " F7." + line
+            #line = line.replace("Apple", "Orange")
         sys.stdout.write(line)
 
 fileinput.close()
