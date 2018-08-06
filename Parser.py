137fd31a7cfff19c1c1e51d975a5a3bb59f1e8c4
Blade2021 <Blade2021@live.com>
2018-08-06 15:09:31 -0400

    Nightly Build

--- Parser.py
+++ Parser.py
@@ -2,37 +2,41 @@ import fileinput
 import sys
 import configparser
 
-
 config = configparser.ConfigParser()
 config.read('data.ini')
-maxValue = int(config['DEFAULT']['KitAmount'])
 
 x = 0
 y = 0
 rate = 0
 value = "Z-"
-slowrate = str(config['DEFAULT']['SlowRate'])
-fastrate = str(config['DEFAULT']['FastRate'])
-
+slowrate = str('F' + config['DEFAULT']['SlowRate'])
+fastrate = str('F' + config['DEFAULT']['FastRate'])
 
 with fileinput.input(files="testfile.txt", backup="test.bak", inplace=1) as file:
     for line in file:
+        if(file.filelineno() == 1):
+            sys.stdout.write(line)
+            continue
+        parentCheck = line.find("(")
+        if parentCheck == 1:
+            continue
+
         line = line.replace("G23", "G03")
         line = line.replace("G22", "G02")
+        line = line.replace("F 0684", "F2.")
         pcodeCheck = line.find('P')
         if pcodeCheck >= 1:
             line = line[0:pcodeCheck]
             line += "\n"
 
         if value in line:
-            y = file.filelineno()+1
+            y = file.filelineno() + 1
             feedCheck = line.find('F')
             if feedCheck >= 1:
                 line = line[0:feedCheck]
-            #line = feedCheck(line)
             line = line.rstrip('\n')
             if rate >= 1:
-                line += slowrate +"\n"
+                line += slowrate + "\n"
                 rate = 0
             else:
                 line += "\n"
@@ -40,13 +44,10 @@ with fileinput.input(files="testfile.txt", backup="test.bak", inplace=1) as file
             feedCheck = line.find('F')
             if feedCheck >= 1:
                 line = line[0:feedCheck]
-            #line = feedCheck(line)
             if rate == 0:
                 line = line.rstrip('\n')
-                line += fastrate +"\n"
+                line += fastrate + "\n"
                 rate = 1
-            #line = " F7." + line
-            #line = line.replace("Apple", "Orange")
         sys.stdout.write(line)
 
 fileinput.close()
